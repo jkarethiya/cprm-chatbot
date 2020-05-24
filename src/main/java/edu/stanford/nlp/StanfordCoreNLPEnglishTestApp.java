95dbfb9ce99cff2cd5fc30e11ffe771ec06ed913
Jitendra Arethiya <jkarethiya@users.noreply.github.com>
2020-05-24 19:17:27 +0530

    stanford lib version upgrade to 4.0.0

--- src/main/java/edu/stanford/nlp/StanfordCoreNLPEnglishTestApp.java
+++ src/main/java/edu/stanford/nlp/StanfordCoreNLPEnglishTestApp.java
@@ -2,17 +2,21 @@ package edu.stanford.nlp;
 
 import java.io.*;
 import java.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.stanford.nlp.io.*;
 import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.util.*;
 
 /** app for testing if Maven distribution is working properly */
 
-public class StanfordCoreNLPEnglishTestApp
-{
-    public static void main(String[] args) throws IOException, ClassNotFoundException
-    {
-        String[] englishArgs = new String[]{"-file", "sample-english.txt", "-outputFormat", "text", "-props", "english.properties"};
-        StanfordCoreNLP.main(englishArgs);
-    }
+public class StanfordCoreNLPEnglishTestApp {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		String[] englishArgs = new String[] { "-file", "sample-english.txt", "-outputFormat", "text", "-props",
+				"english.properties" };
+		StanfordCoreNLP.main(englishArgs);
+	}
 }
